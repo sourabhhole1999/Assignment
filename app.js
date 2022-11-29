40572087b95a8b60641000b340fc240ff7e93bde
sourabhhole1999 <sourabhhole1999@gmail.com>
2022-11-29 19:47:32 +0530

    Assignment-3

--- app.js
+++ app.js
@@ -1,61 +1,22 @@
-const express = require("express");
-const fs = require('fs');
-const PORT = 8000;
-const app = express();
-
+const express=require('express');
+const exphbs=require('express-handlebars')
+const PORT=9999;
+const app=express();
+app.engine('handlebars',exphbs.engine())
+app.set('view engine','handlebars');
+app.set('views','./views');
+app.use(express.static('static'))
 app.use(express.json());
-app.use(express.urlencoded({extended:false}));
-
-app.set('view engine', 'pug');
-app.set('views', './views');
-
-app.use(express.static('static'));
-
-app.get('/', (req, res) => {
-    res.render('index')
+app.use(express.urlencoded({extended:false}))
+//routes 
+const mainRoute=require('./routes/mainRoutes');
+const userRoute=require('./routes/userRoutes');
+app.use("/",mainRoute);
+app.use("/users",userRoute);
+app.use("*",(req,res)=>{
+    res.render("notfound");
 })
-app.get('/about', (req, res) => {
-    res.render('about');
-})
-app.get('/gallery', (req, res) => {
-    res.render('gallery');
-})
-app.get('/services', (req, res) => {
-    res.render('services');
-})
-app.get('/contact', (req, res) => {
-    res.render('contact');
-
-})
-app.post('/data', (req, res) => {
-    let name = req.body.name;
-    let email = req.body.email;
-    let pass = req.body.pass;
-    let age = req.body.age;
-    let city = req.body.city;
-    let mobile = req.body.mobile;
-
-    let data = name+","+email+","+ pass+","+age+","+city+","+mobile +"\n" ;
-    
-    
-    fs.appendFile('file.txt', data, err => {
-        if(err) throw err;
-    })
-
-    // res.send('<h1> Data Added Successfully...!!!');
-    res.render("success");
-})
-
-app.get('/details', (req, res) => {
-    var array = fs.readFileSync('file.txt','UTF-8').split("\n");
-    const file = array.map(ele=>{
-        const d = ele.split(',');
-        return {name:d[0],email:d[1], pass:d[2], age:d[3], city:d[4], mobile:d[5]}
-    })
-            res.render('details', {file : file});
-})
-
-app.listen(PORT, err => {
+app.listen(PORT,(err)=>{
     if(err) throw err;
-    else console.log(`Server is run on ${PORT}`);
+    else console.log(`Server work on ${PORT}`)
 })
